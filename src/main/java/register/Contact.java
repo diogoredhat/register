61dbdf8153aaf5ca32ed3d15edd4840add339cf5
Rodrigo Vitor Ribeiro <ribeiro.rvs@hotmail.com>
2018-04-19 16:37:52 -0300

    Create 'register' package and 'Contact' class.

---
+++ src/main/java/register/Contact.java
@@ -0,0 +1,30 @@
+package register;
+
+public class Contact {
+	private Integer tel;
+	private Integer cel;
+	private String email;
+	
+	public Integer getTel() {
+		return tel;
+	}
+	public void setTel(Integer tel) {
+		this.tel = tel;
+	}
+	
+	public Integer getCel() {
+		return cel;
+	}
+	public void setCel(Integer cel) {
+		this.cel = cel;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+}

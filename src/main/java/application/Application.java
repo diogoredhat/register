7ddc4627c1480de61829f2e634cb479730fb138a
Rodrigo Vitor Ribeiro <ribeiro.rvs@hotmail.com>
2018-05-02 16:38:36 -0300

    Include some MySQL config.

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -20,30 +20,31 @@ public class Application {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
+
 		Contact c = new Contact("Contact Name Test", "11122233344");
 		Date d = new Date();
 		User p = new User("User Name Test", d, 'M', "55566677788", c);		
-		
+
 		p.addContact(c);
-		
-		//System.out.println(p.toString());
-		
+
+		System.out.println(p.toString());
+
+		System.out.println("2");
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("PU");
-		
+
 		EntityManager em = emf.createEntityManager();
-		
+
 		List<User> user = em.createQuery("FROM user", User.class).getResultList();
-		
+
 		for(User u: user) {
 			System.out.println(u.toString());
 		}
-		
-		
+
+
 		em.getTransaction();
 		em.persist(p);
 		em.getTransaction().commit();
-		
+
 		em.close();
 		emf.close();
 	}

dab186869555a2ad21403fb17adfda818fec7814
Rodrigo Vitor Ribeiro <ribeiro.rvs@hotmail.com>
2018-05-02 16:51:00 -0300

    Clear the hibernate connection code.

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -3,9 +3,7 @@
  */
 package application;
 
-import java.util.Date;
 import java.util.List;
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -21,15 +19,6 @@ public class Application {
 	 */
 	public static void main(String[] args) {
 
-		Contact c = new Contact("Contact Name Test", "11122233344");
-		Date d = new Date();
-		User p = new User("User Name Test", d, 'M', "55566677788", c);		
-
-		p.addContact(c);
-
-		System.out.println(p.toString());
-
-		System.out.println("2");
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("PU");
 
 		EntityManager em = emf.createEntityManager();
@@ -39,11 +28,6 @@ public class Application {
 		for(User u: user) {
 			System.out.println(u.toString());
 		}
-
-
-		em.getTransaction();
-		em.persist(p);
-		em.getTransaction().commit();
 
 		em.close();
 		emf.close();

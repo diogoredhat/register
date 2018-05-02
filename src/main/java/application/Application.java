3ea3118d8918baba4f46f1915f787fc21563b851
Rodrigo Vitor Ribeiro <ribeiro.rvs@hotmail.com>
2018-05-02 17:17:33 -0300

    Include dialect in the persistence.xml file.

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -19,7 +19,7 @@ public class Application {
 	 */
 	public static void main(String[] args) {
 
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("PU");
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("default");
 
 		EntityManager em = emf.createEntityManager();
 
